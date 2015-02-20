bf163fb947cc2e29f84be4c652bf65f6df729958
THP <todderick@gmail.com>
2015-02-20 15:02:42 -0500

    does everything but update fully

--- app.js
+++ app.js
@@ -12,11 +12,18 @@ slack.api('rtm.start', function(err, response) {
 
     ws.on('message', function cb(data) {
 	var msg = JSON.parse(data);
-	if(msg.user == 'U03BGBD92' && msg.type == 'message') {
+	if(msg.type == 'message') {
 	    var msgText = msg.text;
 	    if(msgText.toLowerCase().indexOf("it's been") >= 0 || msgText.toLowerCase().indexOf("its been") >= 0) {
-		slack.api('chat.upate', {token: apiToken, ts:msg.ts,channel:msg.channel,});
-		msgText.toLowerCase().replace(/it's been|its been/g, ":musical_note: It's Been :musical_note:")
+		console.log(msgText.toLowerCase().replace(/it's been|its been/g, ":musical_note: It's Been :musical_note:"));
+		slack.api('chat.upate', 
+		     {
+			  token: apiToken, 
+	         	  ts:msg.ts,
+		          channel:msg.channel,
+		          text:msgText.toLowerCase().replace(/it's been|its been/g, ":musical_note: It's Been :musical_note:")
+                     }
+		);
 	    }
 	}
     });
